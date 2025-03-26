a14c2c9adb84d550091c9044db6736dafeb2021b
tamnm22 <minhtam221220@gmail.com>
2025-03-26 10:26:29 +0900

    Deleted all directories name Debug

--- GPIO_driver/Debug/Src/main.d
+++
@@ -1,7 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32f1.h \
- C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32_gpio.h \
- C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32f1.h
-C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32f1.h:
-C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32_gpio.h:
-C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32f1.h:
--- GPIO_driver/Debug/Tam_drivers/Src/stm32_gpio.d
+++
@@ -1,5 +0,0 @@
-Tam_drivers/Src/stm32_gpio.o: ../Tam_drivers/Src/stm32_gpio.c \
- C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32_gpio.h \
- C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32f1.h
-C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32_gpio.h:
-C:/Users/TAMRD/Documents/STM32_HAL/GPIO_driver/Tam_drivers/Inc/stm32f1.h:
